dbe09eebcf537b57a421ebfb46c9c53e792704da
AmruthaPJ <97897284+AmruthaPJ@users.noreply.github.com>
2025-11-14 00:09:52 +0530

    feat(SCRUM-25): add /metrics endpoint and tests

--- src/backend/test/metrics.test.js
+++ src/backend/test/metrics.test.js
@@ -1,7 +1,7 @@
 const request = require("supertest");
 const app = require("../server");
 
-describe("SCRUM-24: /metrics endpoint", () => {
+describe("SCRUM-25: /metrics endpoint", () => {
   it("should return cache statistics with required fields", async () => {
     const res = await request(app).get("/metrics");
     expect(res.statusCode).toBe(200);
